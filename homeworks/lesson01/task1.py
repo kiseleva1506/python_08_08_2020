e2e40c46dfde1421dc3c5dd8280250590fdf7cab
kiseleva15 <kiseleva15@gmail.com>
2020-08-08 14:15:52 +0300

    task 1

---
+++ homeworks/lesson01/task1.py
@@ -0,0 +1,20 @@
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран.
+
+var_str1 = 'first string "1"'
+var_str2 = "second string"
+var_str3 = """third string,
+    long string"""
+
+print(var_str1)
+print(var_str2)
+print(var_str3)
+
+var_int = int(input('enter int '))
+print(var_int)
+print(type(var_int))
+
+var_float = float(input('enter float '))
+print(var_float)
+print(type(var_float))
