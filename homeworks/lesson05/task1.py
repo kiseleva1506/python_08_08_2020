1c6323a1a48cdc94cafc16aed5f28a93f56aac0b
Mary <kiseleva15@gmail.com>
2020-08-21 13:43:17 +0200

    task 1

---
+++ homeworks/lesson05/task1.py
@@ -0,0 +1,15 @@
+""" Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+Об окончании ввода данных свидетельствует пустая строка. """
+
+from pathlib import Path
+
+file_name = Path(__file__).parent.joinpath('text_file.txt')
+print(file_name)
+
+with file_name.open('w') as f:
+    print('Введите строки. Для прекращения введите пустую строку')
+    while True:
+        line = input()
+        if not len(line):
+            break
+        f.write(line + '\n')
